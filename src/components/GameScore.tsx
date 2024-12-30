8fbefd337db45c2d11c29d0fdcba33b96d9e540e
apak0 <muhampak25@gmail.com>
2024-12-30 13:12:57 +0300

    score component adjsuted for score changes

--- src/components/GameScore.tsx
+++ src/components/GameScore.tsx
@@ -6,10 +6,9 @@ interface GameScoreProps {
 
 export const GameScore: React.FC<GameScoreProps> = ({ score }) => (
   <div className="text-center mr-20">
-    
-    <div className="inline-block bg-white rounded-full px-8 py-3 shadow-lg">
+    <div className="inline-block bg-white rounded-full px-2 py-3 shadow-lg w-32">
       <p className="text-xl font-semibold">
-        Score: <span className="text-indigo-600">{score}</span>
+        Score: <span className="text-indigo-600 min-w-[2ch] inline-block text-center">{score}</span>
       </p>
     </div>
   </div>
